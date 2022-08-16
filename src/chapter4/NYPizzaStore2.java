3a02260bbafaf6857e0d987570f8492df9d9d045
hhh <gks0405@koreatech.ac.kr>
2022-08-16 19:52:43 +0900

    팩토리패턴

---
+++ src/chapter4/NYPizzaStore2.java
@@ -0,0 +1,29 @@
+package chapter4;
+
+public class NYPizzaStore2 extends PizzaStore{
+    public Pizza orderPizza(String type){
+        Pizza pizza;
+        pizza = createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+    @Override
+    protected Pizza createPizza(String item) {
+        Pizza pizza = null;
+        PizzaIngredientFactory ingredientFactory = new NYPizzaIngredientFactory();
+        if(item.equals("cheese")){
+            pizza = new CheesePizza(ingredientFactory);
+            pizza.setName("뉴욕 스타일 치즈 피자");
+
+        }else if(item.equals("clam")){
+            pizza = new ClamPizza(ingredientFactory);
+            pizza.setName("뉴욕 스타일 조개 피자");
+        }
+        return pizza;
+    }
+}
--- src/chapter4/Pizza.java
+++ src/chapter4/Pizza.java
@@ -5,20 +5,16 @@ import java.util.List;
 
 public abstract class Pizza {
     String name;
-    String dough;
-    String sauce;
+    Dough dough;
+    Sauce sauce;
+    Veggies veggies[];
+    Cheese cheese;
+    Pepperoni pepperoni;
+    Clams clams;
 
-    List<String> toppings = new ArrayList<String>();
-    void prepare(){
-        System.out.println("준비중: "+name);
-        System.out.println("도우를 돌리는 중...");
-        System.out.println("소스를 뿌리는 중...");
-        System.out.println("토핑을 올리는 중...");
+    abstract  void prepare();
 
-        for (String topping : toppings) {
-            System.out.println(" "+topping);
-        }
-    }
+
     void bake(){
         System.out.println("175도에서 25분간 굽기");
     }
@@ -30,5 +26,11 @@ public abstract class Pizza {
     }
     public  String getName(){
         return name;
+    }
+    void setName(String name){
+        this.name=name;
+    }
+    public String toString(){
+
     }
 }
